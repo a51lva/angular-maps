1a329a083855b633375ad65f9cf3fcc2e60876c0
Acilio Silva <eng.acilio@gmail.com>
2019-04-15 17:10:24 +0100

    Added ABM - angular bootstrap material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 
 import { AgmCoreModule } from '@agm/core';
 import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    MDBBootstrapModule.forRoot(),
     AppRoutingModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB-L9pOYUZqlx7fQLmEkHSfcsE__AuqD-I'
     }),
-    AgmJsMarkerClustererModule
+    AgmJsMarkerClustererModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
