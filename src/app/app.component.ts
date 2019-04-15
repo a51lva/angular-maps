1dbb570d2df389303bbce8ff32a085fb8d9f4d42
Acilio Silva <eng.acilio@gmail.com>
2019-04-15 16:00:29 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-maps';
+  title: string = 'Angular Maps';
+
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+
+  coordinates= [
+    {
+      "lat":"-20.31601562",
+      "lon":"-47.87713101"
+    },
+    {
+      "lat":"28.43503199",
+      "lon":"-96.49844316"
+    },
+    {
+      "lat":"-20.6360047",
+      "lon":"-177.62016748"
+    },
+    {
+      "lat":"23.44454082",
+      "lon":"-116.57702738"
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB-L9pOYUZqlx7fQLmEkHSfcsE__AuqD-I'
+    }),
+    AgmJsMarkerClustererModule
   ],
   providers: [],
   bootstrap: [AppComponent]
